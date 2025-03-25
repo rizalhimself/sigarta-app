d3431f7c1abf69b99d0afbc788dd92753be6fc9d
rizalhimself <rizalhimself@gmail.com>
2025-03-25 08:04:39 +0700

    add data-keluarga/search to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\KeluargaController;
+use App\Http\Controllers\PendudukController;
 use Illuminate\Support\Facades\Route;
 
+
+//rute api data-penduduk
+Route::get('/data-penduduk/search', [PendudukController::class, 'search']);
+
+// rute api data-keluarga
 Route::post('/data-keluarga/store', [KeluargaController::class, 'store']);
 Route::get('/data-keluarga/search', [KeluargaController::class, 'search']);
 
